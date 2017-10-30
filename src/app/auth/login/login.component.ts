05c6b59970216c786efc0bbf953f226f5a3fb994
ershad <ershad@gmail.com>
2017-10-31 02:33:04 +0600

    Polishing

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Login, LoginStatus} from './login';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  alertStyle = '';
+  loginStatus = new LoginStatus('', '');
+  model = new Login('', '');
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
+  onLogin() {
+    this.initLogin();
+    this.authService.login(this.model)
+      .subscribe((status: LoginStatus) => {
+        this.loginStatus = status;
+        if (status.code === 'FAILURE') {
+          this.alertStyle = 'alert alert-danger';
+        }
+      });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  private initLogin() {
+    this.alertStyle = '';
+    this.loginStatus.code = '';
+    this.loginStatus.message = '';
+  }
 }
